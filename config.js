8e8c2e2888020b2b60d126a402881362d0928603
Danilo Dimitrievski <dimitrievski.danilo@gmail.com>
2021-03-19 00:08:39 +0100

    fix

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
     dbURI: "mongodb+srv://user:darkwire@motiviti-tst.1ijz4.mongodb.net/unesco-sites?retryWrites=true&w=majority",
     port: 3003,
-    clientUrl: "https://unesco-sites.000webhostapp.com/"
+    clientUrl: "https://unesco-sites.000webhostapp.com"
 }
